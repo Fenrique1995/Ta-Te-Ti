e49f636fe8acf76b96bcc123a3716b173b77851f
DESKTOP-M2JOLBR\Red Robin <fenrique310@gmail.com>
2022-07-12 20:26:34 -0300

    codigo de javascript, declaraciones de constantes, variables y funciones. Es simple lo que hace es tomar en vase de eventListeners los movimientos del jugador y cambiar los turnos para que cada uno tenga la oportunidad de jugar y hacer su movida. dependiendo del resultado se dictara un mensaje con la victoria de uno u otro jugador o de empate

--- index.js
+++ index.js
@@ -1,38 +1,107 @@
 // ============== Constantes ================ //
-const STATUS_DISPLAY = document.querySelector('.restart');
-const GAME_STATE = ["","","","","","","","",""];
-const WINNINGS = [
-                [0, 1, 2],
-                [3, 4, 5],
-                [6, 7, 8],
-                [0, 3, 6],
-                [1, 4, 7],
-                [2, 5, 8],
-                [0, 4, 8],
-                [2, 4, 6]
-                ];
-const WIN_MESSAGE = () => {'El jugador ${jugador} ha ganado!!!'};
-const DRAW_MESSAGE = () => {'El juego termino en un empate!'};
-const PLAYER_TURN = () => {'Turno del jugador ${jugador}'};
+const STATUS_DISPLAY = document.querySelector('.notificacion'),
+  GAME_STATE = ["", "", "", "", "", "", "", "", ""],
+  WINNINGS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ],
+  WIN_MESSAGE = () => `El jugador ${currentPlayer} ha ganado!`,
+  DRAW_MESSAGE = () => `El juego ha terminado en empate!`,
+  CURRENT_PLAYER_TURN = () => `Turno del jugador ${currentPlayer}`
 // ============== Variables ================ //
-let gameActive = true;
-let jugador = "O";
+let gameActive = true,
+  currentPlayer = "O"
 // ============== Funciones ================ //
-let main = () => {
-    handleStatusDisplay(PLAYER_TURN());
-    listeners();
-}
-let listeners = () => {
-    document.querySelector('.contenedor-juego').addEventListener('click', handleCellClick);
-    document.querySelector('.restart').addEventListener('click', handleRestartGame); 
-}
-function handleStatusDisplay(message) {
-    STATUS_DISPLAY.innerHTML = message;
-}
-let handleRestartGame = () => {
-    gameActive = true;
-    jugador = "X";
-    rstartGameState();
-    handleStatusDisplay(PLAYER_TURN());
-    document.querySelectorAll('.casilla').forEach(cell => cell.innerHTML = "");
-}
+function main() {
+    handleStatusDisplay(CURRENT_PLAYER_TURN())
+    listeners()
+  }
+  
+  function listeners() {
+    document.querySelector('.contenedor-juego').addEventListener('click', handleCellClick)
+    document.querySelector('.restart').addEventListener('click', handleRestartGame)
+  }
+  
+  function handleStatusDisplay(message) {
+    STATUS_DISPLAY.innerHTML = message
+  }
+  
+  function handleRestartGame() {
+    gameActive = true
+    currentPlayer = "X"
+    restartGameState()
+    handleStatusDisplay(CURRENT_PLAYER_TURN())
+    document.querySelectorAll('.casilla').forEach(cell => cell.innerHTML = "")
+  }
+  
+  function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target
+    if (clickedCell.classList.contains('casilla')) {
+      const clickedCellIndex = Array.from(clickedCell.parentNode.children).indexOf(clickedCell)
+      if (GAME_STATE[clickedCellIndex] !== '' || !gameActive) {
+        return false
+      }
+  
+      handleCellPlayed(clickedCell, clickedCellIndex)
+      handleResultValidation()
+    }
+  }
+  
+  function handleCellPlayed(clickedCell, clickedCellIndex) {
+    GAME_STATE[clickedCellIndex] = currentPlayer 
+    clickedCell.innerHTML = currentPlayer 
+  }
+  
+  function handleResultValidation() {
+    let roundWon = false
+    for (let i = 0; i < WINNINGS.length; i++) { 
+      const winCondition = WINNINGS[i] 
+      let position1 = GAME_STATE[winCondition[0]],
+        position2 = GAME_STATE[winCondition[1]],
+        position3 = GAME_STATE[winCondition[2]] 
+  
+      if (position1 === '' || position2 === '' || position3 === '') {
+        continue; 
+      }
+      if (position1 === position2 && position2 === position3) {
+        roundWon = true
+        break
+      }
+    }
+  
+    if (roundWon) {
+      handleStatusDisplay(WIN_MESSAGE())
+      gameActive = false
+      return
+    }
+  
+    let roundDraw = !GAME_STATE.includes("") 
+    
+    if (roundDraw) {
+      handleStatusDisplay(DRAW_MESSAGE())
+      gameActive = false
+      return
+    }
+  
+    handlePlayerChange()
+  }
+  
+  function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X"
+    handleStatusDisplay(CURRENT_PLAYER_TURN())
+  }
+  
+  function restartGameState() {
+    let i = GAME_STATE.length
+    while (i--) {
+      GAME_STATE[i] = ''
+    }
+  }
+  
+  main();
